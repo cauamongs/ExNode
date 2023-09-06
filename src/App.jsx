657e20884c54298a4c66c18b1919cb92b2e0a22b
cauamongs <rm552178@fiap.com.br>
2023-09-05 22:22:20 -0300

    Finalizando o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
+import {Outlet} from 'react-router-dom'
 import Menu from './components/Menu'
 import Rodape from './components/Rodape'
-import {Outlet} from 'react-router-dom'
 
 function App(){
 
--- src/main.jsx
+++ src/main.jsx
@@ -36,3 +36,9 @@ const router = createBrowserRouter([
 
 }
 ])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+)
