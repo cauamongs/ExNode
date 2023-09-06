92446e9b8e1511e38da09042ed91ed5a3873c82a
cauamongs <rm552178@fiap.com.br>
2023-09-05 22:08:54 -0300

    Criando Rodape.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Menu from './components/Menu'
-import Rodape from './components/rodape'
+import Rodape from './components/Rodape'
 import {Outlet} from 'react-router-dom'
 
 function App(){
