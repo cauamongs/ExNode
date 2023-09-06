a4e847f47edcd71d1fcb0bc39b5d9c9c7259436a
cauamongs <rm552178@fiap.com.br>
2023-09-05 21:57:01 -0300

    Configurando rotas utilizando o método createBrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -13,3 +13,26 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path:"/produtos",
+        element: <Produtos/>
+      },
+      {
+        path:"/produtos/editar/:id",
+        element: <EditarProdutos/>
+      }
+    ]
+
+}
+])
